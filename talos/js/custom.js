577a73499ceba4ad6b306c877624fa6dcb2f7515
edisetiawan <edi2992@gmail.com>
2016-10-24 19:51:37 +0700

    versi 1

---
+++ talos/js/custom.js
@@ -0,0 +1,110 @@
+(function($) { "use strict";
+
+	
+	//Navigation	
+
+	$('ul.slimmenu').on('click',function(){
+			var width = $(window).width(); 
+			if ((width <= 1200)){ 
+			$(this).slideToggle(); 
+		}	
+	});				
+	$('ul.slimmenu').slimmenu(
+			{
+			resizeWidth: '1200',
+			collapserTitle: '',
+			easingEffect:'easeInOutQuint',
+			animSpeed:'medium',
+			indentChildren: true,
+			childrenIndenter: '&raquo;'
+	});
+
+	
+	//Home text fade on scroll	
+	
+	$(window).scroll(function () { 
+        var $Fade = $('.fade-elements');
+        //Get scroll position of window 
+        var windowScroll = $(this).scrollTop();
+        //Slow scroll and fade it out 
+        $Fade.css({
+            'margin-top': -(windowScroll / 0) + "px",
+            'opacity': 1 - (windowScroll / 400)
+        });
+    });	
+	
+	/* Scroll Too */
+	
+			$(window).load(function(){"use strict";
+				
+				/* Page Scroll to id fn call */
+				$("ul.slimmenu li a,a[href='#top'],a[data-gal='m_PageScroll2id']").mPageScroll2id({
+					highlightSelector:"ul.slimmenu li a",
+					offset: 78,
+					scrollSpeed:800,
+					scrollEasing: "easeInOutCubic"
+				});
+				
+				/* demo functions */
+				$("a[rel='next']").click(function(e){
+					e.preventDefault();
+					var to=$(this).parent().parent("section").next().attr("id");
+					$.mPageScroll2id("scrollTo",to);
+				});
+				
+			});	
+
+
+
+	
+	$(document).ready(function() {
+
+		//Tooltip
+
+		$(".tipped").tipper();
+	
+	
+		//Parallax
+		
+		$('.parallax-1').parallax("50%", 0.4); 
+		$('.parallax-mask').parallax("50%", 0.2); 
+		$('.parallax-2').parallax("50%", 0.4); 
+	
+	});	
+
+	
+  })(jQuery); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+	
